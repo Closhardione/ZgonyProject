db40203025a58baa574b26efc4edb1c973054484
aston <olekston@gmail.com>
2023-05-10 00:10:23 +0200

    prawie 3

--- src/DeathCauseStatistic.java
+++ src/DeathCauseStatistic.java
@@ -14,7 +14,6 @@ public class DeathCauseStatistic {
     public static DeathCauseStatistic fromCsvFile(String line){
         String[] args = line.split(",");
         DeathCauseStatistic output = new DeathCauseStatistic();
-        output.deathCode=args[0].trim();
         for(int i = 0; i<20;i++){
             if(args[i+2].equals("-")){
                 output.deathCounter[i]=0;
@@ -26,7 +25,16 @@ public class DeathCauseStatistic {
         }
         return output;
     }
-
+    public AgeBracketDeaths getAgeBracketDeaths(int age){
+        if(age>95){
+            age = 95;
+        }
+        int start=age/5*5 ;
+        int end =(age/5*5)+4;
+        int index = age/5;
+        AgeBracketDeaths ageBracketDeaths = new AgeBracketDeaths(start,end,deathCounter[index]);
+        return ageBracketDeaths;
+    }
     public class AgeBracketDeaths{
         public final int young;
         public final int old;
